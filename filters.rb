2eb3acbf260643bed9deca825e3d6c41e1c0db3a
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-06-19 21:52:46 -0700

    Implement filters

--- filters.rb
+++ filters.rb
@@ -2,15 +2,45 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
-  end
+  found = nil
+  @candidates.each {|c| found = c if c[:id] == id }
+  return found
+end
   
-  def experienced?(candidate)
-    # Your code Here
-  end
+def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end
+
   
-  def qualified_candidates(candidates)
+def github_points?(candidate)
+  candidate[:github_points] >= 100
+end
+
+def languages?(candidate)
+  candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+end
+
+def applied_in_time?(candidate)
+  candidate[:date_applied] > 15.days.ago.to_date
+end
+
+def age?(candidate)
+  candidate[:age] >= 18
+end
+
+def qualified_candidates(candidates)
     # Your code Here
+  qualified_candidates = candidates.select do |candidate|
+    experienced?(candidate) &&
+    github_points?(candidate) &&
+    languages?(candidate) &&
+    applied_in_time?(candidate) &&
+    age?(candidate)
   end
-  
-  # More methods will go below
+end
+
+def ordered_by_qualifications(candidates)
+  worst_candidates = candidates.sort_by { |candidate| [candidate[:years_of_experience], candidate[:github_points]]}
+  best_candidates = worst_candidates.reverse()
+  return best_candidates
+end
--- main.rb
+++ main.rb
@@ -6,4 +6,5 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+
+pp ordered_by_qualifications(@candidates)
